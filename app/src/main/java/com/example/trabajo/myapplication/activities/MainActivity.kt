05b627b8225105673105f6b0e9d25476795b4cb0
Pablo Hidalgo <pabloracer_1992@hotmail.com>
2018-06-07 12:07:44 -0400

    agregado datos para recuperar varios tipos de datos y crear JSON

--- app/src/main/java/com/example/trabajo/myapplication/activities/MainActivity.kt
+++ app/src/main/java/com/example/trabajo/myapplication/activities/MainActivity.kt
@@ -1,10 +1,14 @@
-package com.example.trabajo.myapplication
+package com.example.trabajo.myapplication.activities
 
 import android.app.ActivityManager
 import android.content.Context
 import android.os.*
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
+import com.example.trabajo.myapplication.R
+import com.example.trabajo.myapplication.models.OneData
+import com.google.gson.Gson
+import org.json.JSONObject
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,16 +16,33 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Log.d("RAM USAGE", "Actualmente se esta usando ${currentAppRamUsage()} MB de memoria RAM")
-        Log.d("RAM FREE", "Hay ${freeRamMemorySize()} MB libre de memoria RAM")
-        Log.d("RAM TOTAL", "Hay ${totalRamMemorySize()} MB total de memoria RAM")
-        Log.d("AVAILABLE MEMORY", "Hay ${formatSize(getAvailableInternalMemorySize())} disponible de memoria de almacenamiento")
-        Log.d("USED MEMORY", "Hay ${formatSize(getTotalInternalMemorySize() - getAvailableInternalMemorySize())} usado de memoria de almacenamiento")
-        Log.d("TOTAL MEMORY", "Hay ${formatSize(getTotalInternalMemorySize())} total de memoria de almacenamiento")
+        var oneData: OneData
 
-        Log.d("APP NAME", "El nombre de la app es: ${getApplicationName(this)}")
-        Log.d("APP PACKAGE NAME", "El nombre de la app es: ${getAppPackageName()}")
+        oneData = OneData()
+        oneData.appName = getApplicationName(this)
+        //oneData.appPkgName = getAppPackageName()
+        oneData.usedRam = currentAppRamUsage()
+        oneData.freeRam = freeRamMemorySize()
+        oneData.totalRam = totalRamMemorySize()
+        oneData.freeMemory = getAvailableInternalMemorySize()
+        oneData.usedMemory = (getTotalInternalMemorySize() - getAvailableInternalMemorySize())
+        oneData.totalMemory = getTotalInternalMemorySize()
 
+        Log.d("RAM USAGE", "Actualmente se esta usando ${oneData.usedRam} MB de memoria RAM")
+        Log.d("RAM FREE", "Hay ${oneData.freeRam} MB libre de memoria RAM")
+        Log.d("RAM TOTAL", "Hay ${oneData.totalRam} MB total de memoria RAM")
+        Log.d("AVAILABLE MEMORY", "Hay ${oneData.freeMemory} MB disponible de memoria de almacenamiento")
+        Log.d("USED MEMORY", "Hay ${oneData.usedMemory} MB usado de memoria de almacenamiento")
+        Log.d("TOTAL MEMORY", "Hay ${oneData.totalMemory} MB total de memoria de almacenamiento")
+
+        Log.d("APP NAME", "El nombre de la app es: ${oneData.appName}")
+        Log.d("APP PACKAGE NAME", "El nombre de la app es: ${oneData.appPkgName}")
+
+        //TODO agregar version android aparato
+        //TODO listar todas app con nombre com.retailsbs.
+        //
+
+        generateJson(oneData)
 
     }
 
@@ -34,7 +55,7 @@ class MainActivity : AppCompatActivity() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT)
             res += memInfo.totalPrivateClean.toLong()
 
-        return res
+        return res / 1024L
     }
 
     private fun freeRamMemorySize(): Long {
@@ -52,51 +73,51 @@ class MainActivity : AppCompatActivity() {
         return mi.totalMem / 1048576L
     }
 
-    fun externalMemoryAvailable(): Boolean {
+    private fun externalMemoryAvailable(): Boolean {
         return android.os.Environment.getExternalStorageState() == android.os.Environment.MEDIA_MOUNTED
     }
 
-    fun getAvailableInternalMemorySize(): Long {
+    private fun getAvailableInternalMemorySize(): Long {
         val path = Environment.getDataDirectory()
         val stat = StatFs(path.path)
         val blockSize = stat.blockSize.toLong()
         val availableBlocks = stat.availableBlocks.toLong()
-        return availableBlocks * blockSize
+        return (availableBlocks * blockSize) / 1048576L
     }
 
-    fun getTotalInternalMemorySize(): Long {
+    private fun getTotalInternalMemorySize(): Long {
         val path = Environment.getDataDirectory()
         val stat = StatFs(path.path)
         val blockSize = stat.blockSize.toLong()
         val totalBlocks = stat.blockCount.toLong()
-        return totalBlocks * blockSize
+        return (totalBlocks * blockSize) / 1048576L
     }
 
     fun getAvailableExternalMemorySize(): Long {
-        if (externalMemoryAvailable()) {
+        return if (externalMemoryAvailable()) {
             val path = Environment.getExternalStorageDirectory()
             val stat = StatFs(path.path)
             val blockSize = stat.blockSize.toLong()
             val availableBlocks = stat.availableBlocks.toLong()
-            return availableBlocks * blockSize
+            availableBlocks * blockSize
         } else {
-            return 0
+            0
         }
     }
 
     fun getTotalExternalMemorySize(): Long {
-        if (externalMemoryAvailable()) {
+        return if (externalMemoryAvailable()) {
             val path = Environment.getExternalStorageDirectory()
             val stat = StatFs(path.path)
             val blockSize = stat.blockSize.toLong()
             val totalBlocks = stat.blockCount.toLong()
-            return totalBlocks * blockSize
+            totalBlocks * blockSize
         } else {
-            return 0
+            0
         }
     }
 
-    fun formatSize(size: Long): String {
+    private fun formatSize(size: Long): String {
         var size = size
         var suffix: String? = null
 
@@ -119,13 +140,27 @@ class MainActivity : AppCompatActivity() {
         return resultBuffer.toString()
     }
 
-    fun getApplicationName(context: Context): String {
+    private fun getApplicationName(context: Context): String {
         val applicationInfo = context.applicationInfo
         val stringId = applicationInfo.labelRes
         return if (stringId == 0) applicationInfo.nonLocalizedLabel.toString() else context.getString(stringId)
     }
 
-    fun getAppPackageName(): String {
+    private fun getAppPackageName(): String {
         return applicationContext.packageName
     }
+
+    private fun generateJson(oneData: OneData): String? {
+
+        var gson : Gson
+
+        gson = Gson()
+
+        Log.d("JSON TAG",gson.toJson(oneData))
+
+        return gson.toJson(oneData)
+
+    }
+
+
 }
---
+++ app/src/main/java/com/example/trabajo/myapplication/models/OneData.kt
@@ -0,0 +1,19 @@
+package com.example.trabajo.myapplication.models
+
+class OneData {
+
+    var usedRam : Long? = null
+    var freeRam : Long? = null
+    var totalRam : Long? = null
+    var usedMemory : Long? = null
+    var totalMemory : Long? = null
+    var freeMemory : Long? = null
+    var usedExtMemory : Long? = null
+    var availableExtMemory : Long? = null
+    var freeExtMemory : Long? = null
+
+    var appName : String? = null
+    var appPkgName : String? = null
+
+
+}
